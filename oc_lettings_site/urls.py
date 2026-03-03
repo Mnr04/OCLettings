45eb83645986eed54391a93f497e788751d3adfe
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-03-03 13:43:04 +0100

    correction

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('lettings/', include('lettings.urls', namespace='lettings')), #
-    path('profiles/', include('profiles.urls', namespace='profiles')), #
-    path('admin/', admin.site.admin_site.urls),
+    path('', views.index, name='index'),
+    path('lettings/', include('lettings.urls', namespace='lettings')),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('admin/', admin.site.urls),
 ]
