abdfed29d1b12907f991becbf205e55d3cc8ca21
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-03-14 09:17:57 +0100

    correction error

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,7 +6,6 @@ from django.urls import path, include
 from . import views
 
 
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls', namespace='lettings')),
