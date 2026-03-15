71109e77b29f2bdcd387b9a61122b8ed5ddb6fad
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-03-15 11:07:55 +0100

    read the docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -3,7 +3,10 @@ import sys
 import django
 
 
-sys.path.insert(0, os.path.abspath('../..'))
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, '../..'))
+sys.path.insert(0, project_root)
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'oc_lettings_site.settings'
 
 django.setup()
