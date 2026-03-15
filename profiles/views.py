b3e55ec8401649ec09105014f0c96e7a676c6b73
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-03-15 11:21:53 +0100

    check

--- profiles/views.py
+++ profiles/views.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 def index(request):
-    """Displays a list of all user profiles. Test automatisation"""
+    """Displays a list of all user profiles."""
     logger.info("La page index des Profiles a été consultée.")
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
