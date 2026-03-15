b22195dcee23fc1b60939b8845958a0b2216d089
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-03-15 18:38:46 +0100

    add explanatory comments to files

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -2,18 +2,24 @@ import os
 import sys
 import django
 
-
+# Add the project root
 sys.path.insert(0, os.path.abspath('../..'))
 
+# Set the Django settings module
 os.environ['DJANGO_SETTINGS_MODULE'] = 'oc_lettings_site.settings'
+
+# Add a fake secret key to avoid Django errors during the build
 os.environ.setdefault('SECRET_KEY', 'false')
 
+# Start Django to allow Sphinx to read our models and views
 django.setup()
 
+# Project information
 project = 'OCLettings'
 copyright = '2026, JR'
 author = 'JR'
 
+# Enable autodoc to read docstrings from our Python files
 extensions = [
     'sphinx.ext.autodoc',
 ]
@@ -21,4 +27,5 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+# Use the Read the Docs visual theme
 html_theme = 'sphinx_rtd_theme'
