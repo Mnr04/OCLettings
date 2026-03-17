6f9aa4d42b7c7e031b4e7e72f8704e1e78624069
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2026-03-17 14:55:23 +0100

    add log

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -3,8 +3,13 @@ Views for the main application oc_lettings_site.
 This module contains the view for the home page.
 """
 from django.shortcuts import render
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
     """Displays the home page of the application."""
+    logger.info("La page d'accueil principale a été consultée.")
     return render(request, 'index.html')
